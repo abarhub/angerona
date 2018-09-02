29dc31d4776762153273f244304e4896ae72cd8a
abarhub <abarhub@github.com>
2018-09-02 21:41:17 +0200

    deplacement dans 2 modules

---
+++ angerona-core/src/main/java/org/abarhub/angerona/core/coffrefort/Message.java
@@ -0,0 +1,23 @@
+package org.abarhub.angerona.core.coffrefort;
+
+public class Message {
+
+	private String message;
+	private byte[] messageCrypte;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public byte[] getMessageCrypte() {
+		return messageCrypte;
+	}
+
+	public void setMessageCrypte(byte[] messageCrypte) {
+		this.messageCrypte = messageCrypte;
+	}
+}
---
+++ angerona-core/src/test/java/org/abarhub/angerona/core/test/TestConvHex.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.abarhub.angerona.core.test;
+
+import org.abarhub.angerona.core.utils.Tools;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author abarret
+ */
+public class TestConvHex {
+
+	// TODO add test methods here. The name must begin with 'test'. For example:
+	@Test
+	public void test1() {
+		byte[] tab1, tab2;
+
+		tab1 = new byte[]{1, 2, 3};
+		tab2 = new byte[]{1, 2, 3};
+		assertTrue(Tools.egaux(tab1, tab2));
+
+		tab1 = new byte[]{1, 2, 3};
+		tab2 = new byte[]{1, 2, 4};
+		assertTrue(!Tools.egaux(tab1, tab2));
+
+		tab1 = new byte[]{1, 2, 3};
+		tab2 = new byte[]{2, 2, 3};
+		assertTrue(!Tools.egaux(tab1, tab2));
+
+		tab1 = new byte[]{1, 2, 3};
+		tab2 = new byte[]{1, 2, 3, 4};
+		assertTrue(!Tools.egaux(tab1, tab2));
+
+		tab1 = new byte[]{1, 2, 3};
+		tab2 = null;
+		assertTrue(!Tools.egaux(tab1, tab2));
+
+		tab1 = null;
+		tab2 = new byte[]{1, 2, 3};
+		assertTrue(!Tools.egaux(tab1, tab2));
+
+		tab1 = null;
+		tab2 = null;
+		assertTrue(Tools.egaux(tab1, tab2));
+
+		tab1 = new byte[]{9, 9, 9, 9};
+		tab2 = new byte[]{7, 7, 7, 7};
+		assertTrue(!Tools.egaux(tab1, tab2));
+
+		tab1 = new byte[]{1, 2, 3};
+		tab2 = new byte[]{1, 2, 3};
+		assertTrue(Tools.egaux(tab1, tab2));
+	}
+
+	@Test
+	public void test2() throws UnsupportedEncodingException {
+		byte[] tab1, tab2;
+		String s;
+
+		tab1 = new byte[]{1, 2, 3};
+		s = Tools.convHexString(tab1);
+		assertNotNull(s);
+		assertTrue(s.length() == 6);
+		assertEquals("010203", s);
+		tab2 = Tools.convHexByte(s);
+		assertTrue(Tools.egaux(tab1, tab2));
+	}
+
+	@Test
+	public void test3() throws UnsupportedEncodingException {
+		byte[] tab1, tab2;
+		String s;
+
+		tab1 = new byte[]{6, 2, -8, 9, 127, -128, 115, 19, -69};
+		s = Tools.convHexString(tab1);
+		assertNotNull(s);
+		assertTrue(s.length() == tab1.length * 2);
+		assertEquals("0602f8097f807313bb", s);
+		tab2 = Tools.convHexByte(s);
+		assertTrue(Tools.egaux(tab1, tab2));
+	}
+
+	@Test
+	public void test4() throws UnsupportedEncodingException {
+		byte[] tab1, tab2;
+		String s;
+		int i;
+		byte min, max;
+
+		min = Byte.MIN_VALUE;
+		max = Byte.MAX_VALUE;
+		tab1 = new byte[max - min + 1];
+		//i=tab1[256];
+		i = 0;
+		for (byte b = min; b <= max; b++) {
+			tab1[i] = b;
+			i++;
+			if (b == max)
+				break;
+		}
+		s = Tools.convHexString(tab1);
+		assertNotNull(s);
+		assertTrue(s.length() == tab1.length * 2);
+		//assertEquals("0602f8097f807313bb",s);
+		tab2 = Tools.convHexByte(s);
+		assertTrue(Tools.egaux(tab1, tab2));
+	}
+
+	@Test
+	public void test5() throws UnsupportedEncodingException {
+		byte[] tab1, tab2;
+		String s;
+		int nb = 20;
+		byte min, max;
+
+		for (int i = 0; i < nb; i++) {
+			tab1 = Tools.generate_random_bytes(350);
+			s = Tools.convHexString(tab1);
+			assertNotNull(s);
+			assertTrue(s.length() == tab1.length * 2);
+			//assertEquals("0602f8097f807313bb",s);
+			tab2 = Tools.convHexByte(s);
+			assertTrue(Tools.egaux(tab1, tab2));
+		}
+	}
+}
